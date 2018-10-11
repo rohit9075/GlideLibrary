ea65af2abdd1a804c0122f6188e28f63669f7d7b
Rohit Kumar <rohit9075@gmail.com>
2018-10-11 10:05:34 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/com/glidelibrary/MainActivity.java
@@ -0,0 +1,33 @@
+package com.rohit.com.glidelibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonDownloadImage;
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.imageView);
+
+        mButtonDownloadImage = findViewById(R.id.button);
+        mButtonDownloadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GlideApp.with(MainActivity.this)
+                        .load("https://www.mayflower.in/uploads/product/gallery/thumbs/400_400_20170620174927255028.jpg")
+                        .into(mImageView);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/rohit/com/glidelibrary/MyAppGlideModule.java
@@ -0,0 +1,10 @@
+package com.rohit.com.glidelibrary;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
